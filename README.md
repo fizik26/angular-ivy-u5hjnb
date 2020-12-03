018936304331e970b0412a5f74a6d1c7b036976e
fizik26 <thibaut.bouldoire@gmail.com>
2020-12-03 14:24:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-u5hjnb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-u5hjnb)
---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component, VERSION } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  name = "Angular 11";
+
+  public userName = "Thibaut Bouldoire";
+  public avgNoOfBooks = 9.5;
+
+  getReadBooksAvg() {
+    return Math.round(this.avgNoOfBooks);
+  }
+  getAverageColorState() {
+    return this.avgNoOfBooks < 5;
+}
+
+public averageValueState = {
+"text-valid": this.avgNoOfBooks >= 5,
+"text-error": this.avgNoOfBooks < 5
+};
+}
